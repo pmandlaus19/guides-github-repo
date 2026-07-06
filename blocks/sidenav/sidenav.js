4545d1829930e834724c7c975266353f49a63eaf
pmandlaus_19 <pmandlaus@adobe.com>
2026-07-06 12:40:20 +0530

    uploading sidenav to: https://api.github.com/repos/pmandlaus19/guides-github-repo/contents/blocks/sidenav/sidenav.js

--- blocks/sidenav/sidenav.js
+++ blocks/sidenav/sidenav.js
@@ -1,6 +1,6 @@
 import { migrateTree } from "../utils.js";
-const treeData = []
-const mapTitle = "testGit"
+const treeData = [{"displayName":"testgit_3","url":"contents/testgitlab/testgit-3"},{"displayName":"testgit_2","url":"contents/testgit/testgit-2"},{"displayName":"testgit_1","url":"contents/testgit/testgit-1"}]
+const mapTitle = "newMap"
 const isDesktop = window.matchMedia("(min-width: 900px)");
 
 function expandHeirarchy(element, root) {
