405a4f7c57281cc4c556535f36c64985620ffd3f
pmandlaus_19 <pmandlaus@adobe.com>
2026-09-09 14:42:04 +0530

    Uploading sidenav data to: https://api.github.com/repos/pmandlaus19/guides-github-repo/contents/blocks/sidenav/sidenav_data.js

--- blocks/sidenav/sidenav_data.js
+++ blocks/sidenav/sidenav_data.js
@@ -1,1 +1,1 @@
-export default [{"displayName":"glossary-map","isMap":true,"children":[],"_key":"GUID-0f83a3f0-d794-4874-8b17-6e14a134ca52"}]
+export default [{"displayName":"Video content Topic","url":"contents/multimedia-report-data/media-topic"},{"displayName":"Image content testing ","url":"contents/multimedia-report-data/image-content-testing-"},{"displayName":"indirect Topic","url":"contents/multimedia-report-data/indirect-topic"}]
