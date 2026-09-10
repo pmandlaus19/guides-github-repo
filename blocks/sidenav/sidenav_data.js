8e823bfc496c6761440e5f691ac0db92eca3002a
pmandlaus_19 <pmandlaus@adobe.com>
2026-09-10 19:58:05 +0530

    Delete blocks/sidenav/sidenav_data.js

--- blocks/sidenav/sidenav_data.js
+++
@@ -1,1 +0,0 @@
-export default [{"displayName":"Table 5: Table with multiple merged cells","url":"contents/guid-8d9b8d83-2a54-47b4-825b-9230da1095be"},{"displayName":"there","url":"contents/guid-8b8fe8a4-0a17-4548-bb0d-fc4194164ffc"}]
