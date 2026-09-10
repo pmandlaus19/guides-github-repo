a59c648670e8dc8cb708a77b7ad5362935bd3f45
pmandlaus_19 <pmandlaus@adobe.com>
2026-09-10 20:16:28 +0530

    Uploading sidenav (2 file(s), git-data CAS) to guides-github-repo

---
+++ blocks/sidenav/sidenav.js
@@ -0,0 +1,167 @@
+import { migrateTree } from "../utils.js";
+const treeData = [{"displayName":"Product Guide","isMap":true,"children":[{"displayName":"level1.dita","url":"contents/testfolderstructure/level1"},{"displayName":"Quick Start Guide","url":"contents/sidenav-test-filesets/topics/quick-start"},{"displayName":"Getting Started","url":"","children":[{"displayName":"Installing the Product","url":"contents/sidenav-test-filesets/getting-started/installation"},{"displayName":"Your First Project","url":"contents/sidenav-test-filesets/getting-started/first-project"},{"displayName":"Configuration","url":"","children":[{"displayName":"Advanced Tuning","url":"","children":[{"displayName":"Caching Strategies","url":"contents/sidenav-test-filesets/getting-started/configuration/advanced-tuning/caching"}],"isMap":true,"_key":"GUID-62ba26a6-0cdf-4cc3-a352-607454f5972c"}],"isMap":true,"_key":"GUID-2ca0f421-6fc2-4cc4-9400-9d220f9b8c48"},{"displayName":"Legal Notice","url":"contents/sidenav-test-filesets/shared/legal-notice"}],"isMap":true,"_key":"GUID-54ccecfa-72a5-4e4e-98d8-2279e26e1db8"},{"displayName":"Reference","url":"","children":[{"displayName":"API Reference","url":"contents/sidenav-test-filesets/reference/api-reference"},{"displayName":"CLI Reference","url":"contents/sidenav-test-filesets/reference/cli-reference"}]},{"displayName":"appendix-no-title.ditamap","url":"","children":[{"displayName":"Configuration Basics","url":"contents/sidenav-test-filesets/getting-started/configuration/config-basics"},{"displayName":"Glossary of Terms","url":"contents/sidenav-test-filesets/appendix/glossary"}],"isMap":true,"_key":"GUID-294b39cc-86a0-446c-a33a-4084c3d5d88e"}],"_key":"GUID-1f1b4a7f-8c01-454c-b1b6-b8873099fb3d"}]
+const mapTitle = "guides-github-repo-main"
+const isDesktop = window.matchMedia("(min-width: 900px)");
+
+function expandHeirarchy(element, root) {
+  if (element === root) return;
+  let parent = element.parentElement;
+  parent.classList.remove("closed");
+  expandHeirarchy(parent, root);
+}
+
+function expandSelection(parent) {
+  let queryString = window.location.search;
+  let params = new URLSearchParams(queryString);
+  let id = params.get("expand");
+  let element = document.getElementById(`sidenav-li-${id}`);
+  if (!element) return;
+  element.classList.add("selected");
+  expandHeirarchy(element, parent);
+  element.scrollIntoView();
+}
+
+function scrollSidenavSelectionToView() {
+  const element = document.querySelector('.sidenav-list-item.selected')
+  const sidenavContainer = document.getElementsByClassName("sidenav-container")[0];
+  if(!element) return
+  if (element.offsetTop < sidenavContainer.scrollTop || element.offsetTop + element.offsetHeight > sidenavContainer.scrollTop + sidenavContainer.clientHeight) {
+    sidenavContainer.scrollTo({
+      top: Math.max(element.offsetTop - 110, 0),
+      behavior: 'smooth'
+    });
+  }
+}
+
+
+
+function addResizeBar() {
+  const sidenavContainer = document.getElementsByClassName("sidenav-container")[0];
+  const div = document.createElement("div");
+  div.classList.add('sidenav-resize-bar');
+  let isResizing = false
+  div.addEventListener('mousedown', (evt) => {
+    isResizing = true
+    document.addEventListener('mousemove', function (event) {
+      if (isResizing) {
+        let newWidth = event.pageX - sidenavContainer.offsetLeft;
+        sidenavContainer.style.width = `${newWidth}px`;
+      }
+    })
+  })
+  document.addEventListener('mouseup', function () {
+    if (isResizing) {
+      isResizing = false;
+    }
+  })
+  sidenavContainer.insertAdjacentElement("afterend", div)
+}
+
+function addExpandCollapseButton() {
+  const divWrapper = document.createElement("div");
+  divWrapper.classList.add('title-close-wrapper')
+  const titleSpan = document.createElement("span");
+  titleSpan.classList.add('title-span')
+  titleSpan.textContent = mapTitle
+  const span = document.createElement("span");
+  span.classList.add('sidenav-expand-collapse')
+  span.classList.add('open')
+  const sidenavContainer = document.getElementsByClassName("sidenav-container")[0];
+  span.addEventListener('click', () => {
+    const isOpen = span.classList.contains('open')
+    const sidenavResizer = document.getElementsByClassName("sidenav-resize-bar")[0];
+    if(!isOpen) {
+      sidenavContainer.classList.remove('collapse-width')
+      sidenavResizer.classList.remove('force-hide')
+    } else {
+      sidenavContainer.classList.add('collapse-width')
+      sidenavResizer.classList.add('force-hide')
+    }
+    span.classList.toggle("open");
+  })
+  divWrapper.append(titleSpan)
+  divWrapper.append(span)
+  sidenavContainer.prepend(divWrapper)
+}
+
+function generateId(prefix, suffix) {
+  if(prefix) {
+      return `${prefix}-${suffix}`
+  }
+  return `${suffix}`
+}
+
+
+window.addEventListener('aem-app-ready', () => {
+  scrollSidenavSelectionToView()
+})
+
+
+function createTree(parent, data, prefix, level) {
+  const ul = document.createElement("ul");
+  ul.classList.add("tree");
+  parent.appendChild(ul);
+  data.forEach((item, idx) => {
+    const li = document.createElement("li");
+    const newPrefix = generateId(prefix, level)
+    const _id = generateId(newPrefix, idx);
+    li.setAttribute("id", `sidenav-li-${_id}`);
+    ul.appendChild(li);
+    const anchor = document.createElement("a");
+    const span = document.createElement("span");
+    span.classList.add("chevron-icon-span");
+    anchor.textContent = item.displayName;
+    anchor.setAttribute("data-li-id", _id);
+    anchor.setAttribute("title", item.displayName);
+    anchor.setAttribute("aria-label", item.displayName);
+    const siteURL =
+      window.location.protocol +
+      "//" +
+      window.location.hostname +
+      (window.location.port ? ":" + window.location.port : "");
+    if (item.url) {
+      let navURL = new URL(item.url, siteURL).href;
+      anchor.setAttribute("href", navURL);
+      anchor.addEventListener("click", (event) => {
+        event.preventDefault();
+        onClick(anchor.getAttribute("data-li-id"), navURL);
+      });
+    }
+    li.classList.add("sidenav-list-item");
+    li.classList.add("closed");
+    if (item.children) {
+      li.classList.add("has-children");
+      const wrapperSpan = document.createElement("span");
+      wrapperSpan.classList.add("chevron-text-wrapper");
+      wrapperSpan.appendChild(span);
+      wrapperSpan.appendChild(anchor);
+      li.appendChild(wrapperSpan);
+      createTree(li, item.children, newPrefix, idx);
+    } else {
+      li.appendChild(anchor);
+    }
+  });
+}
+
+function onClick(id, navURL) {
+  const url = new URL(navURL);
+  url.searchParams.set("expand", id); // set the query parameter
+  window.location.href = url.toString(); // navigate
+}
+
+// Get the treeview element and create the tree
+const treeview = document.getElementsByClassName("sidenav")[0];
+addExpandCollapseButton();
+createTree(treeview, treeData, '', '');
+migrateTree(isDesktop);
+addResizeBar(treeview);
+isDesktop.addEventListener("change", () => migrateTree(isDesktop));
+expandSelection(treeview);
+
+// Add click event listener to each span element
+treeview.querySelectorAll("span").forEach((span) => {
+  span.addEventListener("click", (event) => {
+    // Toggle the "closed" class on the parent li element
+    event.currentTarget.parentNode.classList.toggle("closed");
+  });
+});
